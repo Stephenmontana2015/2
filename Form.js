bdfcd5b1a11f1e937afabe1fcea1f84e07afa01b
Stephenmontana2015 <91220205+Stephenmontana2015@users.noreply.github.com>
2021-09-22 20:04:09 +0000

    Create Form.js

---
+++ Form.js
@@ -0,0 +1,12 @@
+$(function() {
+ // console.log('html form');
+});
+var a=$("body");
+a.css({"paddingLeft":"30px",fontSize:"20px",color:"white"});
+var b=$("input").eq(5);
+b.css({backgroundColor:"lightskyblue",fontSize:"30px",border:"none",color:"#777"});
+var c=$("body");
+c.css("backgroundColor","gray");
+function color(value){
+document.body.style.backgroundColor=value;
+};
